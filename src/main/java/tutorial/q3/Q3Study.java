3b6ab8138b76c01dd1f2341dff6155059c206cb3
Maurício Aniche <mauricioaniche@gmail.com>
2015-09-21 19:22:40 -0300

    using the new approach to not use the persistence mechanism

--- src/main/java/tutorial/q3/Q3Study.java
+++ src/main/java/tutorial/q3/Q3Study.java
@@ -17,16 +17,16 @@ public class Q3Study implements Study {
 	
 	@Override
 	public void execute() {
-		CSVFile csv = new CSVFile("/Users/mauricioaniche/Desktop/tutorial/q3.csv");
 		ModificationsPerFileVisitor visitor = new ModificationsPerFileVisitor();
 		
 		new RepositoryMining()
 			.in(GitRepository.singleProject("/Users/mauricioaniche/Desktop/tutorial/jfreechart-fse"))
 			.through(Commits.all())
-			.process(visitor, csv)
+			.process(visitor)
 			.withThreads(3)
 			.mine();
 		
+		CSVFile csv = new CSVFile("/Users/mauricioaniche/Desktop/tutorial/q3.csv");
 		for(Map.Entry<String, Integer> k : visitor.getFiles().entrySet()) {
 			csv.write(
 				k.getKey(),
